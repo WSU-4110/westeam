f34c9b68e0aec962418e79c1b12cc4725d8c41d7
Richard Breslin <richardbreslin@wayne.edu>
2021-10-11 20:11:58 -0400

    chore: Template Cleanup
    delete template text. added login page

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -3,7 +3,7 @@ import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import { Login } from './components/Login';
 
 import './custom.css'
 
@@ -13,9 +13,9 @@ export default class App extends Component {
   render () {
     return (
       <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
+            <Route exact path='/' component={Home} />
+            <Route exact path='/login' component={Login} />
+            <Route path='/fetch-data' component={FetchData} />
       </Layout>
     );
   }
--- ClientApp/src/components/Login.js
+++ ClientApp/src/components/Login.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
-export class Home extends Component {
-  static displayName = Home.name;
+export class Login extends Component {
+  static displayName = Login.name;
 
   render () {
     return (
       <div>
-        <h1>Hello, world!</h1>
+        <h1>Login Page here</h1>
         
       </div>
     );
