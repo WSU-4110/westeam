7b9eedee526871b67fc0cb9af09728cb74929236
mohammedali101 <gx0340@wayne.edu>
2021-11-16 19:50:55 -0500

    Error1
    Should make an error if user does not enter Steam ID correctly

---
+++ ClientApp/src/components/FIREBASE_API_KEY.js
@@ -0,0 +1,9 @@
+export let firebaseConfig = {
+    apiKey: "AIzaSyBuQHZ9Bau0K1TRnFp1nIjJQJscQodboxw",
+    authDomain: "westeam-644cb.firebaseapp.com",
+    projectId: "westeam-644cb",
+    storageBucket: "westeam-644cb.appspot.com",
+    messagingSenderId: "36394132295",
+    appId: "1:36394132295:web:a59026d1d5d10c5beb2262",
+    measurementId: "G-93YJQHTZYD"
+}
